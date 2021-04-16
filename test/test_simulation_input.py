9c36322a26f48037b00dd9c8d9fd85687e4d040e
Stefan Broennimann <stefan.broennimann@ennos.ch>
2021-04-16 14:58:52 +0200

    added source and test folder

---
+++ test/test_simulation_input.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+
+from src.simulation_input import simulation_input
+
+class test_simulation_input(unittest.TestCase):
+    #setup() runs before the test and is often used to prepare a common environment used in multiple test
+    def setUp(self):
+        self.testinput = simulation_input()     #creates a new object available for testing    
+        self.add_dummy_data(self.testinput)     #prepare a dummy input vector to be used in the tests
+    
+    #adds some dummy data to the provided destination
+    def add_dummy_data(self, dest, size=100):
+        dest.time_hours = np.arange(size)
+        dest.current_A = np.random.rand(size)               
+        dest.ambient_temperature_C = np.ones(size) * 25
+    
+    def testsetup(self):
+        #make sure we do not have an empty testinput
+        self.assertGreater(len(self.testinput.time_hours),1)
+        self.assertGreater(len(self.testinput.current_A),1)
+        self.assertGreater(len(self.testinput.ambient_temperature_C),1)
+        
+    def test_csv(self):
+        self.testinput.write_csv('temp.csv')
+        
+        new_input = simulation_input() 
+        new_input.read_csv('temp.csv')
+        
+        #check that new_input has same data as self.testinput 
+        self.assertTrue((self.testinput.time_hours == new_input.time_hours).all)
+        self.assertTrue((self.testinput.current_A == new_input.current_A).all)
+        self.assertTrue((self.testinput.ambient_temperature_C == new_input.ambient_temperature_C).all)
+        
+   
+        
